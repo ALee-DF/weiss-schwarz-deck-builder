bff0c3dd6c39eb67da72f753b0bd4982aed207a6
Alexander Lee <alex8usa8@gmail.com>
2017-09-13 00:49:56 -0700

    Used JavaScript File to Populate the Booster Packs Section #1

--- deck-builder/main.js
+++ deck-builder/main.js
@@ -1,11 +1,46 @@
 /* eslint-disable no-unused-vars */
-var boosterPacks = [
+var boosterPacksList = [
   {
     id: 'attack-on-titan-bp',
-    image: 'booster-packs/aot-bp.jpg'
+    cover: 'booster-packs/aot-bp.jpg',
+    name: 'Attack on Titan Booster Pack'
   },
   {
     id: 'love-live-bp',
-    image: 'booster-packs/love-live!-bp.jpg'
+    cover: 'booster-packs/love-live!-bp.jpg',
+    name: 'Love Live! Booster Pack'
+  },
+  {
+    id: 'love-live-vol-2-bp',
+    cover: 'booster-packs/love-live!-vol-2-bp.jpg',
+    name: 'Love Live! Vol.2 Booster Pack'
+  },
+  {
+    id: 'sword-art-online-bp',
+    cover: 'booster-packs/sao-bp.jpg',
+    name: 'Sword Art Online Booster Pack'
+  },
+  {
+    id: 'sword-art-online-vol-2-bp',
+    cover: 'booster-packs/sao-vol-2-bp.jpg',
+    name: 'Sword Art Online Vol. 2 Booster Pack'
   }
 ]
+
+var $boosterPacksSection = document.querySelector('#booster-packs-section')
+
+function renderPack(packObject) {
+  var $newPack = document.createElement('button')
+  var $newCover = document.createElement('img')
+  $newCover.className = 'booster-pack-icon'
+  $newCover.src = packObject.cover
+  $newCover.alt = packObject.name
+
+  $newPack.id = packObject.id
+  $newPack.appendChild($newCover)
+  return $newPack
+}
+
+for (var i = 0; i < boosterPacksList.length; i++) {
+  $boosterPacksSection.appendChild(renderPack(boosterPacksList[i]))
+}
