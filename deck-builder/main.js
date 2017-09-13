b1e2e569ced8a3907e61a0b73b8d60dc3d2665a8
Alexander Lee <alex8usa8@gmail.com>
2017-09-13 11:07:30 -0700

    Added the names of the booster packs with the illustrations. Replaced the "div" element with the "section" element to encompass the list of booster packs #1

--- deck-builder/main.js
+++ deck-builder/main.js
@@ -3,27 +3,27 @@ var boosterPacksList = [
   {
     id: 'attack-on-titan-bp',
     cover: 'booster-packs/aot-bp.jpg',
-    name: 'Attack on Titan Booster Pack'
+    name: 'Attack on Titan'
   },
   {
     id: 'love-live-bp',
     cover: 'booster-packs/love-live!-bp.jpg',
-    name: 'Love Live! Booster Pack'
+    name: 'Love Live!'
   },
   {
     id: 'love-live-vol-2-bp',
     cover: 'booster-packs/love-live!-vol-2-bp.jpg',
-    name: 'Love Live! Vol.2 Booster Pack'
+    name: 'Love Live! Vol.2'
   },
   {
     id: 'sword-art-online-bp',
     cover: 'booster-packs/sao-bp.jpg',
-    name: 'Sword Art Online Booster Pack'
+    name: 'Sword Art Online'
   },
   {
     id: 'sword-art-online-vol-2-bp',
     cover: 'booster-packs/sao-vol-2-bp.jpg',
-    name: 'Sword Art Online Vol. 2 Booster Pack'
+    name: 'Sword Art Online Vol. 2'
   }
 ]
 
@@ -32,12 +32,16 @@ var $boosterPacksSection = document.querySelector('#booster-packs-section')
 function renderPack(packObject) {
   var $newPack = document.createElement('button')
   var $newCover = document.createElement('img')
+  var $newName = document.createElement('p')
   $newCover.className = 'booster-pack-icon'
   $newCover.src = packObject.cover
   $newCover.alt = packObject.name
 
+  $newName.textContent = packObject.name
+
   $newPack.id = packObject.id
   $newPack.appendChild($newCover)
+  $newPack.appendChild($newName)
   return $newPack
 }
 
