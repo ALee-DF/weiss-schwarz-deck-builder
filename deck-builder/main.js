8b2b0e0f6ff8702be55142bd05874137030d2ef4
Alexander Lee <alex8usa8@gmail.com>
2017-09-12 23:38:46 -0700

    Created Button using HTML/CSS

---
+++ deck-builder/main.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+var boosterPacks = [
+  {
+    id: 'attack-on-titan-bp',
+    image: 'booster-packs/aot-bp.jpg'
+  },
+  {
+    id: 'love-live-bp',
+    image: 'booster-packs/love-live!-bp.jpg'
+  }
+]
