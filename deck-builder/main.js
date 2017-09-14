87c32d2ef32228e4935f75e45a0e323b014ca050
Alexander Lee <alex8usa8@gmail.com>
2017-09-14 12:56:31 -0700

    Added card images into booster-packs folder #2

--- deck-builder/main.js
+++ deck-builder/main.js
@@ -2,27 +2,27 @@
 var boosterPacksList = [
   {
     id: 'attack-on-titan-bp',
-    cover: 'booster-packs/aot-bp.jpg',
+    cover: 'booster-packs/aot-card-list/aot-bp.jpg',
     name: 'Attack on Titan'
   },
   {
     id: 'love-live-bp',
-    cover: 'booster-packs/love-live!-bp.jpg',
+    cover: 'booster-packs/love-live!-card-list/love-live!-bp.jpg',
     name: 'Love Live!'
   },
   {
     id: 'love-live-vol-2-bp',
-    cover: 'booster-packs/love-live!-vol-2-bp.jpg',
+    cover: 'booster-packs/love-live!-vol-2-card-list/love-live!-vol-2-bp.jpg',
     name: 'Love Live! Vol.2'
   },
   {
     id: 'sword-art-online-bp',
-    cover: 'booster-packs/sao-bp.jpg',
+    cover: 'booster-packs/sao-card-list/sao-bp.jpg',
     name: 'Sword Art Online'
   },
   {
     id: 'sword-art-online-vol-2-bp',
-    cover: 'booster-packs/sao-vol-2-bp.jpg',
+    cover: 'booster-packs/sao-vol-2-card-list/sao-vol-2-bp.jpg',
     name: 'Sword Art Online Vol. 2'
   }
 ]
