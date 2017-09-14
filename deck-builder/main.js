ac3b187e37a2a510b63e5b48246ca0391943d26c
Alexander Lee <alex8usa8@gmail.com>
2017-09-14 11:12:32 -0700

    Added a "CONFIRM" button to lock in booster pack(s) selection #2

--- deck-builder/main.js
+++ deck-builder/main.js
@@ -28,6 +28,8 @@ var boosterPacksList = [
 ]
 
 var $boosterPacksSection = document.querySelector('#booster-packs-section')
+var $confirm = document.querySelector('#confirm')
+var $selectedPacks = []
 
 function renderPack(pack) {
   var $pack = document.createElement('button')
@@ -47,18 +49,31 @@ function renderPack(pack) {
   return $pack
 }
 
+// Populate booster packs list
 for (var i = 0; i < boosterPacksList.length; i++) {
   $boosterPacksSection.appendChild(renderPack(boosterPacksList[i]))
 }
 
+// Create Event Listeners for each button in the booster packs list
 var $buttons = document.querySelectorAll('.button-tiles')
-
 for (var j = 0; j < $buttons.length; j++) {
   $buttons[j].addEventListener('click', function (event) {
+    // If booster pack is selected, the button's class is changed to
+    // 'pack-selected' and the selected booster pack is recorded into
+    // $selectedPacks array
     if (event.currentTarget.getAttribute('class') === 'button-tiles') {
+      $selectedPacks.push(event.currentTarget.getAttribute('id'))
       event.currentTarget.setAttribute('class', 'pack-selected')
     }
     else {
+      // If booster pack is unselected, the button's class returns to
+      // 'button-tiles' and the unselected booster pack is removed from
+      // $selectedPacks array.
+      for (var k = 0; k < $selectedPacks.length; k++) {
+        if (event.currentTarget.getAttribute('id') === $selectedPacks[k]) {
+          $selectedPacks.splice(k, 1)
+        }
+      }
       event.currentTarget.setAttribute('class', 'button-tiles')
     }
   })
