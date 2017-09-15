9490be597704f9d13b6d678b6a829f26b396406e
Alexander Lee <alex8usa8@gmail.com>
2017-09-15 00:15:52 -0700

    User can view a list of cards from the selected booster packs #2

--- deck-builder/main.js
+++ deck-builder/main.js
@@ -7,6 +7,7 @@ var boosterPacksList = [
     cards: [
       {
         id: 's35-e001',
+        class: 'attack-on-titan-bp',
         cardName: '"Beyond the Walls" Eren',
         cardNumber: 'AOT/S35-E001',
         expansion: 'Attack on Titan',
@@ -27,8 +28,171 @@ var boosterPacksList = [
           'revealed, search your deck for up to one 《Corps》 character, ' +
           'reveal it to your opponent, and put it into your hand. Shuffle ' +
           'your deck afterwards.',
+        flavorText: 'Take out the Colossal Titan!!',
         previewPic: 'booster-packs/aot-card-list/card-preview/s35-e001-preview.jpg',
         picture: 'booster-packs/aot-card-list/card/s35-e001.jpg'
+      },
+      {
+        id: 's35-e001r',
+        class: 'attack-on-titan-bp',
+        cardName: '"Beyond the Walls" Eren',
+        cardNumber: 'AOT/S35-E001R',
+        expansion: 'Attack on Titan',
+        cardType: 'Character',
+        level: 0,
+        power: 1000,
+        trigger: '-',
+        rarity: 'RRR',
+        side: 'S',
+        color: 'yellow',
+        cost: 0,
+        soul: 1,
+        attribute: ['Corps', 'Weapon'],
+        text: '【CONT】 During your turn, your other character in the middle ' +
+          'position of the center stage gets +1000 power.\n【ACT】 Brainstorm ' +
+          '[(1) 【REST】 this card] Reveal four cards from the top of your ' +
+          'deck, and put them into your waiting room. For each climax ' +
+          'revealed, search your deck for up to one 《Corps》 character, ' +
+          'reveal it to your opponent, and put it into your hand. Shuffle ' +
+          'your deck afterwards.',
+        flavorText: 'This is our chance. Don\'t let it slip away!',
+        previewPic: 'booster-packs/aot-card-list/card-preview/s35-e001r-preview.jpg',
+        picture: 'booster-packs/aot-card-list/card/s35-e001r.jpg'
+      },
+      {
+        id: 's35-e002',
+        class: 'attack-on-titan-bp',
+        cardName: '"Resisting Fate" Armin',
+        cardNumber: 'AOT/S35-E002',
+        expansion: 'Attack on Titan',
+        cardType: 'Character',
+        level: 3,
+        power: 6500,
+        trigger: '1',
+        rarity: 'RR',
+        side: 'S',
+        color: 'yellow',
+        cost: 2,
+        soul: 2,
+        attribute: ['Corps', 'Weapon'],
+        text: '【CONT】 This card cannot be chosen by your opponent\'s ' +
+          'effects.\n【CONT】 Assist All of your 《Corps》 characters in ' +
+          'front of this card get +2000 power and the following ability. ' +
+          '"【CONT】 This card cannot be chosen by your opponent\'s effects."\n' +
+          '【AUTO】 When this card is placed on the stage from your hand, ' +
+          'look at up to X cards from the top of your deck, choose up to ' +
+          'one card from among them, and put it into your hand. Put the rest ' +
+          'into your waiting room. X is equal to the number of 《Corps》' +
+          'characters you have.',
+        flavorText: 'To rise above monsters, we have to abandon our humanity.',
+        previewPic: 'booster-packs/aot-card-list/card-preview/s35-e002-preview.jpg',
+        picture: 'booster-packs/aot-card-list/card/s35-e002.jpg'
+      },
+      {
+        id: 's35-e002s',
+        class: 'attack-on-titan-bp',
+        cardName: '"Resisting Fate" Armin',
+        cardNumber: 'AOT/S35-E002S',
+        expansion: 'Attack on Titan',
+        cardType: 'Character',
+        level: 3,
+        power: 6500,
+        trigger: '1',
+        rarity: 'SR',
+        side: 'S',
+        color: 'yellow',
+        cost: 2,
+        soul: 2,
+        attribute: ['Corps', 'Weapon'],
+        text: '【CONT】 This card cannot be chosen by your opponent\'s ' +
+          'effects.\n【CONT】 Assist All of your 《Corps》 characters in ' +
+          'front of this card get +2000 power and the following ability. ' +
+          '"【CONT】 This card cannot be chosen by your opponent\'s effects."\n' +
+          '【AUTO】 When this card is placed on the stage from your hand, ' +
+          'look at up to X cards from the top of your deck, choose up to ' +
+          'one card from among them, and put it into your hand. Put the rest ' +
+          'into your waiting room. X is equal to the number of 《Corps》' +
+          'characters you have.',
+        flavorText: 'Eren is not a foe of humanity!',
+        previewPic: 'booster-packs/aot-card-list/card-preview/s35-e002s-preview.jpg',
+        picture: 'booster-packs/aot-card-list/card/s35-e002s.jpg'
+      },
+      {
+        id: 's35-e003',
+        class: 'attack-on-titan-bp',
+        cardName: '"Resisting Fate" Eren',
+        cardNumber: 'AOT/S35-E003',
+        expansion: 'Attack on Titan',
+        cardType: 'Character',
+        level: 3,
+        power: 9500,
+        trigger: '1',
+        rarity: 'RR',
+        side: 'S',
+        color: 'yellow',
+        cost: 2,
+        soul: 2,
+        attribute: ['Corps', 'Weapon'],
+        text: '【CONT】If your opponent has a level 3 or higher character, ' +
+          'this card gets -1 level while in your hand.\n【CONT】 This card ' +
+          'gets +500 power for each of your other 《Corps》 characters.\n' +
+          '【AUTO】 This ability activates up to one time per turn. During ' +
+          'the turn that this card is placed on the stage from your hand, ' +
+          'when damage dealt by this card is canceled, you may deal one ' +
+          'damage to your opponent. (This damage may be canceled)',
+        flavorText: 'And before I\'m done with \'em, the Titans will grow to ' +
+          'hate me.',
+        previewPic: 'booster-packs/aot-card-list/card-preview/s35-e003-preview.jpg',
+        picture: 'booster-packs/aot-card-list/card/s35-e003.jpg'
+      },
+      {
+        id: 's35-e003sp',
+        class: 'attack-on-titan-bp',
+        cardName: '"Resisting Fate" Eren',
+        cardNumber: 'AOT/S35-E003SP',
+        expansion: 'Attack on Titan',
+        cardType: 'Character',
+        level: 3,
+        power: 9500,
+        trigger: '1',
+        rarity: 'SP',
+        side: 'S',
+        color: 'yellow',
+        cost: 2,
+        soul: 2,
+        attribute: ['Corps', 'Weapon'],
+        text: '【CONT】If your opponent has a level 3 or higher character, ' +
+          'this card gets -1 level while in your hand.\n【CONT】 This card ' +
+          'gets +500 power for each of your other 《Corps》 characters.\n' +
+          '【AUTO】 This ability activates up to one time per turn. During ' +
+          'the turn that this card is placed on the stage from your hand, ' +
+          'when damage dealt by this card is canceled, you may deal one ' +
+          'damage to your opponent. (This damage may be canceled)',
+        flavorText: '',
+        previewPic: 'booster-packs/aot-card-list/card-preview/s35-e003sp-preview.jpg',
+        picture: 'booster-packs/aot-card-list/card/s35-e003sp.jpg'
+      },
+      {
+        id: 's35-e004',
+        class: 'attack-on-titan-bp',
+        cardName: '"Entrance Ceremony" Annie',
+        cardNumber: 'AOT/S35-E004',
+        expansion: 'Attack on Titan',
+        cardType: 'Character',
+        level: 0,
+        power: 2500,
+        trigger: '-',
+        rarity: 'R',
+        side: 'S',
+        color: 'yellow',
+        cost: 0,
+        soul: 1,
+        attribute: ['Corps', 'Weapon'],
+        text: '【CONT】 If the number of cards in your stock is two or less, ' +
+          'this card gets +1500 power.',
+        flavorText: '',
+        previewPic: 'booster-packs/aot-card-list/card-preview/s35-e004-preview.jpg',
+        picture: 'booster-packs/aot-card-list/card/s35-e004.jpg'
       }
     ]
   },
@@ -45,7 +209,111 @@ var boosterPacksList = [
   {
     id: 'sword-art-online-bp',
     cover: 'booster-packs/sao-card-list/sao-bp.jpg',
-    name: 'Sword Art Online'
+    name: 'Sword Art Online',
+    cards: [
+      {
+        id: 's20-e001',
+        class: 'sword-art-online-bp',
+        cardName: 'Asuna Invites to Party',
+        cardNumber: 'SAO/S20-E001',
+        expansion: 'Sword Art Online',
+        cardType: 'Character',
+        level: 1,
+        power: 4500,
+        trigger: '-',
+        rarity: 'RR',
+        side: 'S',
+        color: 'yellow',
+        cost: 0,
+        soul: 1,
+        attribute: ['Avatar', 'Weapon'],
+        text: '【CONT】 If the number of other 《Avatar》 or 《Net》 ' +
+          'characters you have is three or more, this card gets +1000 power.\n' +
+          '【AUTO】[(1)］ When this card attacks, if a card named ' +
+          '"<<Star Splash>>" is in your climax area, you may pay the cost. ' +
+          'If you do, search your deck for up to one 《Avatar》 or 《Net》 ' +
+          'character, reveal it to your opponent, and put it into your hand. ' +
+          'Shuffle your deck afterwards.',
+        flavorText: 'Then, party up with me. It\'s been forever.',
+        previewPic: 'booster-packs/sao-card-list/card-preview/s20-e001-preview.jpg',
+        picture: 'booster-packs/sao-card-list/card/s20-e001.jpg'
+      },
+      {
+        id: 's20-e001s',
+        class: 'sword-art-online-bp',
+        cardName: 'Asuna Invites to Party',
+        cardNumber: 'SAO/S20-E001s',
+        expansion: 'Sword Art Online',
+        cardType: 'Character',
+        level: 1,
+        power: 4500,
+        trigger: '-',
+        rarity: 'SR',
+        side: 'S',
+        color: 'yellow',
+        cost: 0,
+        soul: 1,
+        attribute: ['Avatar', 'Weapon'],
+        text: '【CONT】 If the number of other 《Avatar》 or 《Net》 ' +
+          'characters you have is three or more, this card gets +1000 power.\n' +
+          '【AUTO】[(1)］ When this card attacks, if a card named ' +
+          '"<<Star Splash>>" is in your climax area, you may pay the cost. ' +
+          'If you do, search your deck for up to one 《Avatar》 or 《Net》 ' +
+          'character, reveal it to your opponent, and put it into your hand. ' +
+          'Shuffle your deck afterwards.',
+        flavorText: 'Kirito, Switch!!',
+        previewPic: 'booster-packs/sao-card-list/card-preview/s20-e001s-preview.jpg',
+        picture: 'booster-packs/sao-card-list/card/s20-e001s.jpg'
+      },
+      {
+        id: 's20-e002',
+        class: 'sword-art-online-bp',
+        cardName: '<<Lightning Flash>> Asuna',
+        cardNumber: 'SAO/S20-E002',
+        expansion: 'Sword Art Online',
+        cardType: 'Character',
+        level: 2,
+        power: 8000,
+        trigger: '1',
+        rarity: 'RR',
+        side: 'S',
+        color: 'yellow',
+        cost: 1,
+        soul: 1,
+        attribute: ['Avatar', 'Weapon'],
+        text: '【CONT】 During your turn, if the character facing this card ' +
+          'is level 3 or higher, this card gets +4000 power and +1 soul. \n' +
+          '【AUTO】 When this card\'s battle opponent becomes 【REVERSE】, ' +
+          'put that character into your opponent\'s stock.',
+        flavorText: 'I\'ll protect you, too. I\'ll protect you forever.',
+        previewPic: 'booster-packs/sao-card-list/card-preview/s20-e002-preview.jpg',
+        picture: 'booster-packs/sao-card-list/card/s20-e002.jpg'
+      },
+      {
+        id: 's20-e002sp',
+        class: 'sword-art-online-bp',
+        cardName: '<<Lightning Flash>> Asuna',
+        cardNumber: 'SAO/S20-E002SP',
+        expansion: 'Sword Art Online',
+        cardType: 'Character',
+        level: 2,
+        power: 8000,
+        trigger: '1',
+        rarity: 'SP',
+        side: 'S',
+        color: 'yellow',
+        cost: 1,
+        soul: 1,
+        attribute: ['Avatar', 'Weapon'],
+        text: '【CONT】 During your turn, if the character facing this card ' +
+          'is level 3 or higher, this card gets +4000 power and +1 soul. \n' +
+          '【AUTO】 When this card\'s battle opponent becomes 【REVERSE】, ' +
+          'put that character into your opponent\'s stock.',
+        flavorText: 'I\'ll protect you, too. I\'ll protect you forever.',
+        previewPic: 'booster-packs/sao-card-list/card-preview/s20-e002sp-preview.jpg',
+        picture: 'booster-packs/sao-card-list/card/s20-e002sp.jpg'
+      }
+    ]
   },
   {
     id: 'sword-art-online-vol-2-bp',
@@ -55,6 +323,7 @@ var boosterPacksList = [
 ]
 
 var $boosterPacksSection = document.querySelector('#booster-packs-section')
+var $cardList = document.querySelector('#card-list')
 var $confirm = document.querySelector('#confirm')
 var $selectedPacks = []
 
@@ -105,3 +374,65 @@ for (var j = 0; j < $buttons.length; j++) {
     }
   })
 }
+
+function renderCard(card) {
+  var $row = document.createElement('tr')
+  var $cardPreview = document.createElement('td')
+  var $cardPreviewPic = document.createElement('img')
+  var $cardNumber = document.createElement('td')
+  var $cardType = document.createElement('td')
+  var $cardName = document.createElement('td')
+  var $color = document.createElement('td')
+  var $colorText = document.createElement('p')
+
+  $cardPreviewPic.className = 'card-preview'
+  $cardPreviewPic.src = card.previewPic
+  $cardPreviewPic.alt = card.cardName
+  $cardPreview.className = 'preview-padding'
+  $cardPreview.appendChild($cardPreviewPic)
+
+  $cardNumber.textContent = card.cardNumber
+  $cardType.textContent = card.cardType
+  $cardName.textContent = card.cardName
+
+  if (card.color === 'yellow') {
+    $colorText.className = 'yellow-card'
+    $colorText.textContent = 'YELLOW'
+  }
+  else if (card.color === 'blue') {
+    $colorText.className = 'blue-card'
+    $colorText.textContent = 'BLUE'
+  }
+  else if (card.color === 'red') {
+    $colorText.className = 'red-card'
+    $colorText.textContent = 'RED'
+  }
+  else if (card.color === 'green') {
+    $colorText.className = 'green-card'
+    $colorText.textContent = 'GREEN'
+  }
+  $color.appendChild($colorText)
+  $row.id = card.id
+  $row.className = card.class
+  $row.appendChild($cardPreview)
+  $row.appendChild($cardNumber)
+  $row.appendChild($cardType)
+  $row.appendChild($cardName)
+  $row.appendChild($color)
+  return $row
+}
+
+// Once the user presses the "Confirm Button", the card list will populate
+// according to the packs selected.
+$confirm.addEventListener('click', function () {
+  $selectedPacks.sort()
+  for (var l = 0; l < $selectedPacks.length; l++) {
+    for (var m = 0; m < boosterPacksList.length; m++) {
+      if ($selectedPacks[l] === boosterPacksList[m].id) {
+        for (var n = 0; n < boosterPacksList[m].cards.length; n++) {
+          $cardList.appendChild(renderCard(boosterPacksList[m].cards[n]))
+        }
+      }
+    }
+  }
+})
